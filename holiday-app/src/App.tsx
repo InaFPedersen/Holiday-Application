f6286664d418d762a38f4d982c2fec85fe98e3eb
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-12-19 12:04:16 +0100

    update: change code to prepare to create Topbar

--- holiday-app/src/App.tsx
+++ holiday-app/src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css'
+
+import { useState } from 'react';
+import Topbar from './components/Topbar/Topbar'
 
 function App() {
+
+  // Global states
+  const [user, setUser] = useState("");
+  const [lightModeActive, setLightModeActive] = useState(true);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [activeLanguage, setActiveLanguage] = useState("Norwegian")
+
   return (
     <div>
-      <h1>Hello world!</h1>
+      <Topbar user={user} setUser={setUser} lightModeActive={lightModeActive} setLightModeActive={setLightModeActive} userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} activeLanguage={activeLanguage} setActiveLanguage={setActiveLanguage} />
+      <h1>Hello {user}!</h1>
     </div>
   )
 }
