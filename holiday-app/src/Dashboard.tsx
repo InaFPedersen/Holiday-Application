e18bb8b125957c7f843c106af5509222f774abf4
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-12-20 13:30:47 +0100

    feat: add placeholders for elements on dashboard

--- holiday-app/src/Dashboard.tsx
+++ holiday-app/src/Dashboard.tsx
@@ -7,16 +7,51 @@ const Dashboard = () => {
   const [user, setUser] = useState('');
   const [lightModeActive, setLightModeActive] = useState(true);
   const [userLoggedIn, setUserLoggedIn] = useState(false);
-  const [activeLanguage, setActiveLanguage] = useState('Norwegian')
+  const [activeLanguage, setActiveLanguage] = useState('Norwegian');
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isLeader, setIsLeader] = useState(false);
 
   return (
     <div className='grid-container'>
       <div className='header'>
         <Topbar user={user} setUser={setUser} lightModeActive={lightModeActive} setLightModeActive={setLightModeActive} userLoggedIn={userLoggedIn} setUserLoggedIn={setUserLoggedIn} activeLanguage={activeLanguage} setActiveLanguage={setActiveLanguage} />
       </div>
+
       <h1 className='welcome-message'>Hello {user}!</h1>
-      <div className='navigation-container'>Test</div>
-      <div className='personal-container'>Test</div>
+
+      <div className='navigation-container'>
+        <div className='box'>
+          {isAdmin ? (
+            <>
+              <button className='icon-button information'>Information</button>
+              <button className='icon-button user-guide'>user-guide</button>
+              <button className='icon-button calendars'>calendars</button>
+              <button className='icon-button holiday-request'>holiday-request</button>
+              <button className='icon-button requests-status'>requests-status</button>
+              <button className='icon-button add-group'>add-group</button>
+              <button className='icon-button submit-feedback'>submit-feedback</button>
+
+              <button className='icon-button admin-page'>Admin page</button>
+              <button className='icon-button view-feedback'>View feedback</button>
+              {isLeader && <button className='icon-button view-holiday-requests'>View holiday request</button>}
+            </>
+          ) : (
+            <>
+              <button className='icon-button information'>Information</button>
+              <button className='icon-button user-guide'>user-guide</button>
+              <button className='icon-button calendars'>calendars</button>
+              <button className='icon-button holiday-request'>holiday-request</button>
+              <button className='icon-button requests-status'>requests-status</button>
+              <button className='icon-button add-group'>add-group</button>
+              <button className='icon-button submit-feedback'>submit-feedback</button>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className='personal-container'>
+        <div className='calendar'>Personal calendar here</div>
+      </div>
     </div>
   )
 }
