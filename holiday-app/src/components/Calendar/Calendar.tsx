5aaf0d9d1f4245f283be08b7065e51570acb60fa
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2025-01-10 13:18:05 +0100

    feat: add a template calender navigation

--- holiday-app/src/components/Calendar/Calendar.tsx
+++ holiday-app/src/components/Calendar/Calendar.tsx
@@ -7,9 +7,39 @@ type Props = {
 const Calendar = ({lightModeActive} : Props) => {
 
   
+  
 
   return (
     <div className={`calendar-container ${lightModeActive ? 'component-light-mode' : 'component-dark-mode'}`}>
+      <div className='calender-header'>
+        {lightModeActive ? 
+          <div className='calendar-navigation'>
+            <div className='arrow-btn'>
+              <img className='arrow-btn-img' src='./icons/arrow-left-icon-light.svg' alt='Light mode Arrow left icon, for going backwards in the calendar' />
+            </div>
+
+            <h2 className='calendar-navigation-text'>JANUARY</h2>
+
+            <div className='arrow-btn'>
+              <img className='arrow-btn-img' src='./icons/arrow-right-icon-light.svg' alt='Light mode Arrow left icon, for going backwards in the calendar' />
+            </div>
+          </div> 
+          : 
+          <div className='calendar-navigation'>
+            <div className='arrow-btn'>
+              <img className='arrow-btn-img' src='./icons/arrow-left-icon-dark.svg' alt='Dark mode Arrow left icon, for going backwards in the calendar' />
+            </div>
+
+            <h2 className='calendar-navigation-text'>JANUARY</h2>
+
+            <div className='arrow-btn'>
+              <img className='arrow-btn-img' src='./icons/arrow-right-icon-dark.svg' alt='Dark mode Arrow left icon, for going backwards in the calendar' />
+            </div>
+          </div>
+        }
+        
+
+      </div>
       <div className='calendar-container'>
       <h1>HERE PERSONAL CALENDAR WILL COME</h1>
       </div>
